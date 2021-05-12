af10d6f50ab4f57689bd049b4425c18d71bb655a
sami-p <sami.peterson14@gmail.com>
2021-05-12 16:04:12 -0500

    Add #kick_out method spec test

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -31,18 +31,18 @@ describe Classroom do
       classroom = Classroom.new('History', 4)
       classroom.add_student('Mike')
       classroom.add_student('Megan')
-      classroom.add_student('Bob')
+      classroom.add_student('Ian')
 
-      expect(classroom.students).to eq ['Mike', 'Megan', 'Bob']
+      expect(classroom.students).to eq ['Mike', 'Megan', 'Ian']
     end
 
     it 'returns a list of uppercased names' do
       classroom = Classroom.new('History', 4)
       classroom.add_student('Mike')
       classroom.add_student('Megan')
-      classroom.add_student('Bob')
+      classroom.add_student('Ian')
 
-      expect(classroom.yell_at_students).to eq ['MIKE', 'MEGAN', 'BOB']
+      expect(classroom.yell_at_students).to eq ['MIKE', 'MEGAN', 'IAN']
     end
   end
 
@@ -67,6 +67,29 @@ describe Classroom do
       classroom.add_student('Alice')
 
       expect(classroom.over_capacity?).to eq(true)
-    end 
+    end
+  end
+
+  # Iteration 4
+
+  context 'Capacity' do
+    it 'can #kick_out student' do
+      classroom = Classroom.new('History', 4)
+      classroom.add_student('Mike')
+      classroom.add_student('Megan')
+      classroom.add_student('Bob')
+      classroom.add_student('James')
+      classroom.add_student('Cat')
+      classroom.add_student('Alice')
+
+      classroom.kick_out
+
+      expect(classroom.over_capacity?).to eq(true)
+
+      classroom.kick_out
+
+      expect(classroom.over_capacity?).to eq(false)
+      expect(classroom.students).to eq(['Bob', 'James', 'Cat', 'Alice'])
+    end
   end
 end
